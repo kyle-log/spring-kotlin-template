0f7bf1b95c37c832db3faea5b1d767299f000fd9
Kyle <cocomo1316@gmail.com>
2023-11-02 10:10:58 +0900

    Remove unused comment

--- src/main/kotlin/com/cocomo/web/Controller.kt
+++ src/main/kotlin/com/cocomo/web/Controller.kt
@@ -3,9 +3,6 @@ package com.cocomo.web
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
-/**
- * @author kyle.kim@daangn.com
- */
 @RestController
 class Controller {
 
